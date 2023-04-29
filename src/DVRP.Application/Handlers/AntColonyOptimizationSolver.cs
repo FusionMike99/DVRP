009034aa531358db1d383a0aaddf2c515eb691b6
Михайло Тезяєв <fusionmike99@gmail.com>
2023-04-29 18:21:41 +0300

    change aco

--- src/DVRP.Application/Handlers/AntColonyOptimizationSolver.cs
+++ src/DVRP.Application/Handlers/AntColonyOptimizationSolver.cs
@@ -6,65 +6,206 @@ namespace DVRP.Application.Handlers;
 
 public class AntColonyOptimizationSolver : IDvrpSolver
 {
-    private DvrpModel _model = null!;
-    private DistanceMatrix _distanceMatrix = null!;
-    private PheromoneMatrix _pheromoneMatrix = null!;
-
     public Algorithm Algorithm => Algorithm.AntColonyOptimization;
 
     public DvrpSolution Solve(DvrpModel model, DvrpSolverParameters parameters)
     {
-        return Solve(model, parameters, null);
+        return Solve(model, parameters);
     }
 
     public DvrpSolution Solve(DvrpModel model, DvrpSolverParameters parameters, DvrpSolution? initialSolution = null)
     {
         if (parameters is not AntColonyParameters antColonyParameters)
         {
-            throw new ArgumentException("The provided parameters must be of type AntColonyParameters.");
+            throw new ArgumentException("Invalid parameters type", nameof(parameters));
         }
 
-        Initialize(model);
-
-        int numberOfAnts = antColonyParameters.NumberOfAnts;
-        int numberOfIterations = antColonyParameters.MaxIterations;
-        double alpha = antColonyParameters.Alpha;
-        double beta = antColonyParameters.Beta;
-        double rho = antColonyParameters.EvaporationRate;
-        double q = antColonyParameters.Q;
+        int numberOfLocations = model.Customers.Count + model.Depots.Count;
+        double[][] pheromoneMatrix = InitializePheromoneMatrix(numberOfLocations);
+        double[][] distanceMatrix = CalculateDistanceMatrix(model);
 
         DvrpSolution? bestSolution = initialSolution;
+        double bestFitness = double.MaxValue;
 
-        int currentVehicle = 0;
-
-        for (int iteration = 0; iteration < numberOfIterations; iteration++)
+        for (int iteration = 0; iteration < antColonyParameters.MaxIterations; iteration++)
         {
-            List<DvrpSolution> antSolutions = new(numberOfAnts);
+            List<DvrpSolution> antSolutions = new();
 
-            for (int ant = 0; ant < numberOfAnts; ant++)
+            for (int ant = 0; ant < antColonyParameters.NumberOfAnts; ant++)
             {
-                Ant antInstance = new(_model, _distanceMatrix, _pheromoneMatrix);
-                DvrpSolution antSolution = antInstance.BuildSolution(alpha, beta, currentVehicle);
-                antSolutions.Add(antSolution);
+                DvrpSolution antSolution = ConstructSolution(model, antColonyParameters, pheromoneMatrix, distanceMatrix);
+                antSolution.CalculateFitness(model.Depots.Count);
 
-                if (bestSolution is null || antSolution.TotalDistance < bestSolution.TotalDistance)
+                if (antSolution.Fitness < bestFitness)
                 {
                     bestSolution = antSolution;
+                    bestFitness = antSolution.Fitness;
+                }
+
+                antSolutions.Add(antSolution);
+            }
+
+            UpdatePheromoneMatrix(pheromoneMatrix, antSolutions, antColonyParameters);
+        }
+
+        return bestSolution ?? new DvrpSolution();
+    }
+
+    private static double[][] InitializePheromoneMatrix(int numberOfLocations)
+    {
+        double initialPheromoneValue = 0.01;
+        double[][] pheromoneMatrix = new double[numberOfLocations][];
+
+        for (int i = 0; i < numberOfLocations; i++)
+        {
+            pheromoneMatrix[i] = new double[numberOfLocations];
+
+            for (int j = 0; j < numberOfLocations; j++)
+            {
+                if (i == j)
+                {
+                    pheromoneMatrix[i][j] = 0;
+                }
+                else
+                {
+                    pheromoneMatrix[i][j] = initialPheromoneValue;
+                }
+            }
+        }
+
+        return pheromoneMatrix;
+    }
+
+    private static double[][] CalculateDistanceMatrix(DvrpModel model)
+    {
+        List<Location> locations = new();
+        locations.AddRange(model.Depots);
+        locations.AddRange(model.Customers);
+
+        int numberOfLocations = locations.Count;
+        double[][] distanceMatrix = new double[numberOfLocations][];
+
+        for (int i = 0; i < numberOfLocations; i++)
+        {
+            distanceMatrix[i] = new double[numberOfLocations];
+
+            for (int j = 0; j < numberOfLocations; j++)
+            {
+                if (i == j)
+                {
+                    distanceMatrix[i][j] = 0;
+                }
+                else
+                {
+                    distanceMatrix[i][j] = locations[i].CalculateDistance(locations[j]);
+                }
+            }
+        }
+
+        return distanceMatrix;
+    }
+
+    private static DvrpSolution ConstructSolution(DvrpModel model, AntColonyParameters parameters, double[][] pheromoneMatrix, double[][] distanceMatrix)
+    {
+        List<Customer> unvisitedCustomers = new(model.Customers);
+        List<Vehicle> vehicles = new(model.Vehicles);
+        List<VehicleRoute> vehicleRoutes = new();
+
+        while (unvisitedCustomers.Count > 0 && vehicles.Count > 0)
+        {
+            Vehicle vehicle = vehicles[0];
+            vehicles.RemoveAt(0);
+
+            VehicleRoute vehicleRoute = new() { Vehicle = vehicle with { } };
+            Location currentLocation = model.Depots.First(d => d.Id == vehicle.DepotId);
+            double remainingCapacity = vehicle.Capacity;
+
+            while (true)
+            {
+                Location? nextLocation = SelectNextLocation(currentLocation, unvisitedCustomers, remainingCapacity, pheromoneMatrix, distanceMatrix, parameters);
+
+                if (nextLocation == null)
+                {
+                    vehicleRoute.Locations.Add(vehicle); // Return to the depot
+                    break;
+                }
+
+                vehicleRoute.Locations.Add(nextLocation);
+                currentLocation = nextLocation;
+
+                if (nextLocation is Customer customer)
+                {
+                    remainingCapacity -= customer.Demand;
+                    unvisitedCustomers.Remove(customer);
                 }
             }
 
-            _pheromoneMatrix.UpdatePheromoneTrails(antSolutions, rho, q);
-
-            currentVehicle = (currentVehicle + 1) % _model.Vehicles.Count;
+            vehicleRoutes.Add(vehicleRoute);
         }
 
-        return bestSolution!;
+        DvrpSolution solution = new() { Routes = vehicleRoutes };
+        return solution;
     }
 
-    private void Initialize(DvrpModel model)
+    private static Location? SelectNextLocation(Location currentLocation, List<Customer> unvisitedCustomers, double remainingCapacity,
+        double[][] pheromoneMatrix, double[][] distanceMatrix, AntColonyParameters parameters)
     {
-        _model = model;
-        _distanceMatrix = new DistanceMatrix(model);
-        _pheromoneMatrix = new PheromoneMatrix(model);
+        List<Customer> feasibleCustomers = unvisitedCustomers.Where(c => c.Demand <= remainingCapacity).ToList();
+
+        if (feasibleCustomers.Count == 0)
+        {
+            return null; // No more customers can be visited with the remaining capacity
+        }
+
+        int currentIndex = currentLocation.Id;
+        double sumPheromoneDistance = feasibleCustomers.Sum(c => Math.Pow(pheromoneMatrix[currentIndex][c.Id], parameters.Alpha) * Math.Pow(1 / distanceMatrix[currentIndex][c.Id], parameters.Beta));
+
+        double randomValue = Random.Shared.NextDouble();
+        double accumulatedProbability = 0;
+
+        foreach (Customer customer in feasibleCustomers)
+        {
+            double probability = (Math.Pow(pheromoneMatrix[currentIndex][customer.Id], parameters.Alpha) * Math.Pow(1 / distanceMatrix[currentIndex][customer.Id], parameters.Beta)) / sumPheromoneDistance;
+            accumulatedProbability += probability;
+
+            if (randomValue <= accumulatedProbability)
+            {
+                return customer;
+            }
+        }
+
+        return feasibleCustomers.Last(); // If no location has been selected yet, choose the last feasible customer
+    }
+
+    private static void UpdatePheromoneMatrix(double[][] pheromoneMatrix, List<DvrpSolution> antSolutions, AntColonyParameters parameters)
+    {
+        int numberOfLocations = pheromoneMatrix.Length;
+
+        // Evaporate pheromones
+        for (int i = 0; i < numberOfLocations; i++)
+        {
+            for (int j = 0; j < numberOfLocations; j++)
+            {
+                pheromoneMatrix[i][j] *= (1 - parameters.EvaporationRate);
+            }
+        }
+
+        // Deposit pheromones based on ant solutions
+        foreach (DvrpSolution solution in antSolutions)
+        {
+            double deltaPheromone = parameters.Q / solution.TotalDistance;
+
+            foreach (VehicleRoute route in solution.Routes)
+            {
+                for (int i = 0; i < route.Locations.Count - 1; i++)
+                {
+                    int currentLocationId = route.Locations[i].Id;
+                    int nextLocationId = route.Locations[i + 1].Id;
+
+                    pheromoneMatrix[currentLocationId][nextLocationId] += deltaPheromone;
+                    pheromoneMatrix[nextLocationId][currentLocationId] += deltaPheromone;
+                }
+            }
+        }
     }
 }
--- src/DVRP.Domain/Entities/AntColonyParameters.cs
+++ src/DVRP.Domain/Entities/AntColonyParameters.cs
@@ -2,10 +2,10 @@
 
 public class AntColonyParameters : DvrpSolverParameters
 {
+    public int MaxIterations { get; set; }
     public int NumberOfAnts { get; set; }
     public double Alpha { get; set; }
     public double Beta { get; set; }
     public double EvaporationRate { get; set; }
-    public int MaxIterations { get; set; }
     public double Q { get; set; }
 }
